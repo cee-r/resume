b0bd244220f942d5d3c90a8edd827f615058a9c2
cee-r <cr.repository@gmail.com>
2018-05-14 15:50:30 -0400

    build update 180514

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ### Notes
 
+* added scroll nav structure and basic set up
+* added sticky/shrink header
+* Added back to top
+* Automatically search for number and wrap span with percentage class  around it
 * Made data-task values dynamic based on # of tasks
 * Refined width style with sass maps and for loops
 * Improve breakdown by making array dynamic
--- js/js.js
+++ js/js.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+
+  // Back to Top
+  $(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+
+	$('#back-to-top').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});
+
+  // Sticky Header
+  var shrink = $(".shrink");
+
+  $(window).scroll(function() {
+		if( $(this).scrollTop() > 0 ) {
+			shrink.addClass("shrink-on");
+		} else {
+			shrink.removeClass("shrink-on");
+		}
+	});
+
+  var headerOffset = $('header').height();
+  $("main").css("margin-top", headerOffset);
+
+  // Slider
   $('#qualifications .ql-slider').slick({
     infinite: true,
     slidesToShow: 3,
@@ -9,26 +38,26 @@ $(document).ready(function(){
     dots: true,
   });
 
-
+  // Breakdown
   // var tasks = [ "task-1", "task-2", "task-3", "task-4", "task-5", "task-6" ];
   var tasks = [];
-  //var tasks = [];
+  var rxp = new RegExp("([0-9]+\.?[0-9]+)", "gm");
 
-  $(".tasks").each(function(i){
+  $(".tasks").each(function(i, v){
 
-     $(this).attr("data-task" , (i + 1))
-
-     tasks.push($(this).attr("class") + "[data-task='" + $(this).data("task") + "']");
-
-  });
-
-  jQuery.each( tasks, function( i, val ) {
-
-    var percentage = $( "." + val + " .percentage" ).text();
-
-    $( "." + val ).addClass( "w-" + percentage );
+    $(this).html($(this).html().replace(rxp, "<span class=\"percentage\">$1</span>"));
+    $(this).attr("data-task" , (i + 1));
+    tasks.push($(this).attr("class") + "[data-task='" + $(this).data("task") + "']");
 
   });
 
-  console.log(tasks);
+  jQuery.each( tasks, function( i, v ) {
+
+    var percentage = $( "." + v + " .percentage" ).text();
+
+    $( "." + v ).addClass( "w-" + percentage );
+
+  });
+
+  //console.log(tasks);
 });
